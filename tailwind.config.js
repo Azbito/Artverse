0dfe0a9ab39d756e573f21e5a689b1bae9b7ae3e
Thiago <101950809+Azbito@users.noreply.github.com>
2023-04-22 18:25:09 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,24 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
     },
+    fontFamily: {
+      kristi: ['Kristi', 'Poppins', 'sans'],
+      julius: ['Julius Sans One', 'Poppins', 'sans']
+    },
+    fontSize: {
+      xl: '5rem',
+      base: '1.5rem'
+    }
   },
-  plugins: [],
+  plugins: []
 }
