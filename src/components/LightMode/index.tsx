234fc5558033183817d81051768d7dc1c07bc7dc
Thiago <101950809+Azbito@users.noreply.github.com>
2023-04-28 22:28:47 -0300

    REFACTOR: Media queries are all done now

--- src/components/LightMode/index.tsx
+++ src/components/LightMode/index.tsx
@@ -10,10 +10,10 @@ export default function LightMode() {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="mt-32 flex items-center justify-around sm:flex-col md:flex-row gap-10">
-        <Image src={MountainMorning} alt="Starry Night" className="rounded-lg md:ml-[5rem] sm:w-[30rem] md:w-[20rem] lg:w-[40rem] 2xl:w-full" width={500} />
+      <div className="mt-32 flex items-center justify-around sm:flex-col lg:flex-row gap-10">
+        <Image src={MountainMorning} alt="Starry Night" className="rounded-lg lg:ml-[5rem] sm:w-[30rem]  md:ml-[0rem] lg:w-[40rem] 2xl:w-full" width={500} />
         <div>
-          <h1 className="font-kristi text-xl text-blue-500 sm:text-center sm:text-[6rem] md:text-start md:text-[5rem] xl:text-[7rem] 2xl:text-[10rem]">Artverse</h1>
+          <h1 className="font-kristi text-xl text-blue-500 sm:text-center sm:text-[6rem] lg:text-start lg:text-[5rem] xl:text-[7rem] 2xl:text-[10rem]">Artverse</h1>
           <p className="font-julius text-base text-black xl:text-[2rem] 2xl:text-[3rem]">Express your emotions. Fell emotions. Be free.</p>
         </div>
       </div>
--- src/components/Offer/index.tsx
+++ src/components/Offer/index.tsx
@@ -3,30 +3,55 @@ import TheLovers from '../../../public/images/the-lovers.jpg'
 import ManInTheOcean from '../../../public/images/man-in-the-ocean.jpg'
 import Night from '../../../public/images/night.jpg'
 import SurrealPainting from '../../../public/images/pxfuel.jpg'
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { LightContext } from "@/contexts/lightContext";
 import Button from "../Button";
 
 export default function Offer() {
   const { isLight } = useContext(LightContext)
 
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 1024) {
+        setIsMobile(true)
+        return
+      }
+      setIsMobile(false)
+    }
+  }, [])
+
   return (
-    <div className="flex items-center justify-around flex-row mt-32 lg:gap-16">
-      {isLight ? <Image src={SurrealPainting} alt="Pxfuel's surrealism" className="w-1/2 lg:w-[15rem] xl:w-[20rem] h-auto object-cover rounded-lg" />
+    <>
+      {!isMobile ?
+        <div className="flex items-center justify-around flex-row mt-32 lg:gap-16">
+          {isLight ? <Image src={SurrealPainting} alt="Pxfuel's surrealism" className="w-1/2 lg:w-[12rem] xl:w-[20rem] h-auto object-cover rounded-lg" />
+            :
+            <Image src={TheLovers} alt="The lovers" className="w-1/2 lg:w-[12rem] xl:w-[15rem] h-auto object-cover rounded-lg" />
+          }
+          <div className="flex items-center justify-center flex-col gap-12">
+            <h1 className={isLight ?
+              "text-blue-600 font-poppins font-bold text-center lg:text-[2rem]" : "dark:text-white font-poppins font-bold text-center xl:text-[3rem] lg:text-lg"}>What do we offer you?</h1>
+            <p className={isLight ? "text-black font-poppins text-base w-27 text-center lg:text-[1rem] 2xl:w-[30rem]" : "dark:text-white font-poppins text-base w-27 text-center 2xl:text-[2rem] 2xl:w-[30rem]"}>A bunch of content about art, history & more.</p>
+            <Button title="Check it out" onClick={() => alert("This project was made to understand local storage. There's no more pages.")} />
+          </div>
+          {isLight ?
+            <Image src={ManInTheOcean} alt="Man In The Ocean" className="w-1/2 lg:w-[12rem] xl:w-[20rem] h-auto object-cover rounded-lg" />
+            :
+            <Image src={Night} alt="Night" className="w-1/2 lg:w-[12rem] xl:w-[15rem] h-auto object-cover rounded-lg" />
+          }
+        </div>
         :
-        <Image src={TheLovers} alt="The lovers" className="w-1/2 lg:w-[15rem] xl:w-[20rem] h-auto object-cover rounded-lg" />
-      }
-      <div className="flex items-center justify-center flex-col gap-12">
-        <h1 className={isLight ?
-          "text-blue-600 font-poppins font-bold text-center xl:text-[3rem] lg: text-lg" : "dark:text-white font-poppins font-bold text-center xl:text-[3rem] lg: text-lg"}>What do we offer you?</h1>
-        <p className={isLight ? "text-black font-poppins text-base w-27 text-center xl:text-[2rem] 2xl:w-[30rem]" : "dark:text-white font-poppins text-base w-27 text-center 2xl:text-[2rem] 2xl:w-[30rem]"}>A bunch of content about art, history & more.</p>
-        <Button title="Check it out" onClick={() => alert("This project was made to understand local storage. There's no more pages.")} />
-      </div>
-      {isLight ?
-        <Image src={ManInTheOcean} alt="Man In The Ocean" className="w-1/2 lg:w-[15rem] xl:w-[20rem] h-auto object-cover rounded-lg" />
-        :
-        <Image src={Night} alt="Night" className="w-1/2 lg:w-[15rem] xl:w-[20rem] h-auto object-cover rounded-lg" />
-      }
-    </div>
+        <div className="flex items-center justify-around flex-row mt-32 lg:gap-16">
+          <div className="flex items-center justify-center flex-col gap-12 p-[4rem] shadow-2xl rounded-xl">
+            <h1 className={isLight ?
+              "text-blue-600 font-poppins font-bold text-center text-[2rem]" : "dark:text-white font-poppins font-bold text-center text-[2rem]"}>What do we offer you?</h1>
+            <p className={isLight ? "text-black font-poppins text-base w-27 text-center lg:text-[1rem] 2xl:w-[30rem]" : "dark:text-white font-poppins text-base w-27 text-center 2xl:text-[2rem] 2xl:w-[30rem]"}>A bunch of content about art, history & more.</p>
+            <Button title="Check it out" onClick={() => alert("This project was made to understand local storage. There's no more pages.")} />
+          </div>
+        </div>}
+    </>
   )
 }
